054be11c6da77c1e77f47d7daa86a929995e709c
qendriimrexhepi <qendriimrexhepi@gmail.com>
2022-08-17 23:02:54 +0200

    Crud Form in ReactJS

---
+++ src/redux/contactReducer.js
@@ -0,0 +1,31 @@
+const initialState = [
+  { id: 0, name: "Qëndrim Rexhepi", email: "qendrim@email.com", phone: 38349420056 },
+  { id: 1, name: "Valdrin Rexhepi", email: "valdrin@gmail.com", phone: 38344717809 },
+];
+
+export const contactReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_CONTACT":
+      state = [...state, action.payload];
+      return state;
+    case "DELETE_CONTACT":
+      const contactFilter = state.filter((contact) =>
+        contact.id === action.payload ? null : contact
+      );
+      state = contactFilter;
+      return state;
+    case "UPDATE_CONTACT":
+      const contactUpdate = state.filter((contact) =>
+        contact.id === action.payload.id
+          ? Object.assign(contact, action.payload)
+          : contact
+      );
+      state = contactUpdate;
+      return state;
+    case "RESET_CONTACT":
+      state = [{ name: null, email: null, phone: null }];
+      return state;
+    default:
+      return state;
+  }
+};
